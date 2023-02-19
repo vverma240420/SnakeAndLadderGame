6af9055a82c133a45c69559b79ab58868a5ec14b
Vivek Verma <vverma240420@gmail.com>
2023-02-19 15:23:13 +0530

    Roll A Dice

--- SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/Program.cs
@@ -4,11 +4,16 @@
     {
         static void Main(string[] args)
         {
-            int position = 0;
+            int position = 0, rollDice;
             int playerOne;
             playerOne = position;
+            Random random = new Random();
+            rollDice = random.Next(1, 7);
             Console.WriteLine("....Snake And Ladder....");
             Console.WriteLine("position of the playerone is =" + playerOne);
+            Console.WriteLine("Rolled dice number is:" + rollDice);
+            Console.ReadLine();
+
         }
     }
 }
