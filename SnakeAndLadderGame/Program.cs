7c3036d975c7e5811e68dc7410d12cbefaa13218
Vivek Verma <vverma240420@gmail.com>
2023-02-19 15:33:39 +0530

    Adding Repeating loop

--- SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/Program.cs
@@ -9,38 +9,52 @@
             const int NoGame = 2;
             int position = 0;
             Random random = new Random();
-            int diceRoll = random.Next(1, 7);
-            Console.WriteLine("Rolled Dice number is:" + diceRoll);
-            int choice = random.Next(0, 3);
 
-
-            switch (choice)
+            while (position < 100)
             {
-                case Snake:
-                    position -= diceRoll;
-                    Console.WriteLine("Its a snake");
-                    if (position < 0)
-                    {
-                        Console.WriteLine("Player 1 position : 0");
-                    }
-                    else
-                    {
+                int diceRoll = random.Next(1, 7);
+                Console.WriteLine("Rolled Dice number is:" + diceRoll);
+                int choice = random.Next(0, 3);
+
+                switch (choice)
+                {
+                    case Snake:
+                        position -= diceRoll;
+                        Console.WriteLine("Its a snake");
+                        if (position < 0)
+                        {
+                            position = 0;
+                            Console.WriteLine("Player 1 position :" + position);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Player 1 position : " + position);
+                        }
+                        break;
+                    case Ladder:
+                        position += diceRoll;
+                        Console.WriteLine("Its a ladder");
+                        if (position >= 100)
+                        {
+                            position = 100;
+                            Console.WriteLine("Player 1 position : " + position);
+                            Console.WriteLine("Player 1 wins");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Player 1 position : " + position);
+                        }
+                        break;
+                    case NoGame:
+                        Console.WriteLine("Its a NoGame");
                         Console.WriteLine("Player 1 position : " + position);
-                    }
-                    break;
-                case Ladder:
-                    position += diceRoll;
-                    Console.WriteLine("Its a ladder");
-                    Console.WriteLine("Player 1 position : " + position);
-                    break;
-                case NoGame:
-                    Console.WriteLine("Its a NoGame");
-                    Console.WriteLine("Player 1 position : " + position);
-                    break;
+                        break;
 
-            }
-            Console.ReadLine();
+                }
+                Console.ReadLine();
 
+            }          
         }
     }
 }
