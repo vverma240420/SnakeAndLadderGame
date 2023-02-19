b9dc7c34be63dfb73a22e56f9ee9c3e34fef549c
Vivek Verma <vverma240420@gmail.com>
2023-02-19 15:18:31 +0530

    Snake And Ladder Program

---
+++ SnakeAndLadderGame/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SnakeAndLadderGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int position = 0;
+            int playerOne;
+            playerOne = position;
+            Console.WriteLine("....Snake And Ladder....");
+            Console.WriteLine("position of the playerone is =" + playerOne);
+            Console.ReadLine();
+
+        }
+    }
+}
