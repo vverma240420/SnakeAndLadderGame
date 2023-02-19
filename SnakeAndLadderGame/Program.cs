3f730ec52de782c1fb23dfddcbb32f2ef468a463
Vivek Verma <vverma240420@gmail.com>
2023-02-19 15:26:09 +0530

    Checking Player Position

--- SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/Program.cs
@@ -4,14 +4,41 @@
     {
         static void Main(string[] args)
         {
-            int position = 0, rollDice;
-            int playerOne;
-            playerOne = position;
+            const int Snake = 0;
+            const int Ladder = 1;
+            const int NoGame = 2;
+            int position = 0;
             Random random = new Random();
-            rollDice = random.Next(1, 7);
-            Console.WriteLine("....Snake And Ladder....");
-            Console.WriteLine("position of the playerone is =" + playerOne);
-            Console.WriteLine("Rolled dice number is:" + rollDice);
+            int diceRoll = random.Next(1, 7);
+            Console.WriteLine("Rolled Dice number is:" + diceRoll);
+            int choice = random.Next(0, 3);
+
+
+            switch (choice)
+            {
+                case Snake:
+                    position -= diceRoll;
+                    Console.WriteLine("Its a snake");
+                    if (position < 0)
+                    {
+                        Console.WriteLine("Player 1 position : 0");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Player 1 position : " + position);
+                    }
+                    break;
+                case Ladder:
+                    position += diceRoll;
+                    Console.WriteLine("Its a ladder");
+                    Console.WriteLine("Player 1 position : " + position);
+                    break;
+                case NoGame:
+                    Console.WriteLine("Its a NoGame");
+                    Console.WriteLine("Player 1 position : " + position);
+                    break;
+
+            }
             Console.ReadLine();
 
         }
